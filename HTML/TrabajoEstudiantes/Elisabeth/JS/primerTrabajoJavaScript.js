22ce8729037b1d576812f74f8bd126826f448f3f
ELISABETH <elisabethospinag@gmail.com>
2020-09-15 23:16:15 -0500

    TrabajosJavaScriptElisa

---
+++ HTML/TrabajoEstudiantes/Elisabeth/JS/primerTrabajoJavaScript.js
@@ -0,0 +1,10 @@
+function sayHello(){
+    alert(" welcom to my form!!");
+}
+
+function confirmDecition(){
+    var preguntar = confirm ("You haven´t saved the changes. \n Are you sure you want to leave?");
+    if(preguntar==true){
+        alert("Good by!!");
+    }
+}
