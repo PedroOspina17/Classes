9c3e04a4f5875684f257ec7346092fd91dfd1d08
pedro ospina <pedroospina17@gmail.com>
2021-02-16 17:37:59 -0500

    Jquery class 3 material and exercises.

--- ClassMaterial/WebPages_FrontEnd/04.Jquery/js/03.GettingFormValues.js
+++ ClassMaterial/WebPages_FrontEnd/04.Jquery/js/03.GettingFormValues.js
@@ -1,20 +1,35 @@
 
 function displayMessage(){
-    var user =  $("#user").val();
-    var user =  $("#userAge").val();
+
+
+    // Inputs
+    var user =  $("#user").val(); //Type text
+    var userAge =  $("#userAge").val(); //Type number
+    var userDOB =  $("#dobDate").val(); //Type date
+
     
-    var doctypeValue = $("#doctype").val();
-    var doctype = $("#doctype option:checked");
+    // Selects
+    var doctypeValue = $("#doctype").val();  // To get only the value
+    // var doctypeValue = $("#doctype option:checked").val(); // Same as before
+    var doctypeText = $("#doctype option:checked").text(); // To get the value and the text
     
-    var gender = $("input[name='gender']:checked").val()
-    var knowledgeList = $('input[name="locationthemes"]:checked');
+    // Radiobuttons
+    var gender = $("input[name='gender']:checked").val(); //To get the value
+    var genderDesc = $("input[name='gender']:checked").attr("description"); //To get the value
+    
+    // Checkboxes
+    var knowledgeList = $('input[name="knowledge"]:checked');
+    console.log(knowledgeList)
     var knowledge = "";
     $.each(knowledgeList, function(index, value) {
 
         console.log(value);
-        // knowledge += value 
+        knowledge += $(value).val() + "\n";
 
      });
 
+    var resultMessage = `user: ${user} \nuserAge: ${userAge}  \ndoctype: ${ doctypeText} (${doctypeValue})\n`;
+    resultMessage += `date of birdh: ${userDOB} \ngender: ${genderDesc} (${gender}) \nknowledge: ${knowledge}`
+    $("#result2").val(resultMessage);
 
 }
