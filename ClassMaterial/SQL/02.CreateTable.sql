4e57856031677b2b6c546d2c105325578bae5703
pedro ospina <pedroospina17@gmail.com>
2021-02-22 21:21:44 -0500

    Class material updates

--- ClassMaterial/SQL/02.CreateTable.sql
+++ ClassMaterial/SQL/02.CreateTable.sql
@@ -11,8 +11,12 @@
                     Time: To store time only. HH/MM/SS.MMMM
                     DateTime: To store date and time. Day/Month/Year HH/MM/SS.MMMM
                     Bit: To store boolean data. (1 / 0) True or false.
-    <<Primary Key>>: To specify that the field is the one that identifies the row. All the values in this field should be unique. 
+    <<NOT NULL>>: To specify that the field is required. It goes after all the restrictions. 
     <<Identity(Start,Increment)>>: To specify that el field is going to be autoincrementable.
+    <<Primary Key>>: To specify that the field is the one that identifies the row. All the values in this field should be unique. 
+                    To create a primary key after the datatype use 'PRIMARY KEY'
+    <<Foreign Keys>>: It is used to reference data from another table. Needs to be specified the table to be referenced and the primary key of the table.
+                    To create a primary key after the datatype use FOREIGN KEY REFERENCES <<TableName>>(<<PrimaryKey>>)
 
 CREATE TABLE <<TableName>>(
     <<FieldName>> <<DataType>> PRIMARY KEY,
@@ -26,5 +30,15 @@ CREATE TABLE <<TableName>>(
 
 CREATE TABLE Country(
     Id INT IDENTITY(1,1) PRIMARY KEY,
-    Name VARCHAR(50)
+    Name VARCHAR(30) NOT NULL,
+    LongName VARCHAR(100),
+    Description VARCHAR(50)
+)
+
+CREATE TABLE Department(
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name VARCHAR(50) NOT NULL,
+    LongName VARCHAR(100),
+    Description VARCHAR(50),
+    IdCountry INT FOREIGN KEY REFERENCES Country(Id)
 )
