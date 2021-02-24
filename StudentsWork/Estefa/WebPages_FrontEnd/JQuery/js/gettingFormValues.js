8f3cf894bfab24fa9115f3a9d65891fffbc62274
Estefania <estefa.rodriguez18@gmail.com>
2021-02-23 21:56:40 -0500

    Create folder

---
+++ StudentsWork/Estefa/WebPages_FrontEnd/JQuery/js/gettingFormValues.js
@@ -0,0 +1,10 @@
+function displayMessage(){
+    var user = $("#user").val();
+    var name = $("#userName").val();
+    var age = $("#userAge").val();
+
+    var dateBirth = $("[name='dobDate']").val();
+    var expirationYear =  $("#ccardYear").val();    
+    var expirationMonth =  $("#ccardMonth").val();
+    
+}
