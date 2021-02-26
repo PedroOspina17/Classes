aac2b9b946c5fa9079fa17c1bdc8d243178f270a
pedro ospina <pedroospina17@gmail.com>
2021-02-25 20:17:47 -0500

    Small modification on 03.Select class material

--- ClassMaterial/SQL/04.SelectStructure.sql
+++ ClassMaterial/SQL/04.SelectStructure.sql
@@ -7,13 +7,13 @@ WHERE <<Condition>> AND/OR <<Condition>>
 ORDER BY <<SortField>> ASC/DESC
 
 <<FieldList>>:	All the fields separated by comma that are going to be displayed. 
-				The star (*) means that all the fields will be displayed.
-<<Table>>:		The table name where the Select will be executed.
+				The star (*) means that all the fields will be displayed. MANDATORY STATEMENT
+<<Table>>:		The table name where the Select will be executed. MANDATORY STATEMENT
 <<Condition>>:  A logic operation similar to the IF operation. 
 				**Need to be an specific true or false question.**
 				Several conditions can be mixed with AND/ OR operators. 
 				ALL THE ENTRIES THAT MEETS ALL THE CONDITIONS ARE GOING TO BE DISPLAYED.
-<<SortField>>:  The field to be used as sorting. 
+<<SortField>>:  The field to be used as sorting. ALWAYS AT THE END OF THE SCRIPT
 				After the field goes the direction. 
 					ASC means ascendence (From small to big) and DESC means descendece (From Big to small). 
 
