2d240e41d3b676624953834bb1049e02da9e1b80
LAPTOP-7ECVFME0\wossa <tatiso0110@hotmail.com>
2021-06-16 17:53:16 -0500

    no message

---
+++ HTML/TrabajoEstudiantes/alexa/clase individual java/pclasejava.js
@@ -0,0 +1,1 @@
+document.write("<h1>hello mundo</h1>");
---
+++ StudentsWork/alexa/clases febrero - copia/misegundotrabajo.js
@@ -0,0 +1,12 @@
+function ocultartexto(){
+    $("input").hide();
+}
+function mostrartexto(){
+    $("input").show();
+}
+function ocultartexto1(){
+$("input[type='number']").hide();
+}
+function mostrarbotones(){
+    $("input").show();
+}
