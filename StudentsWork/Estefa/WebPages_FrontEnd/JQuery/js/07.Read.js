7083f19306adfa8b35152198fbec634f1d64d976
Estefania <estefa.rodriguez18@gmail.com>
2021-07-28 20:01:07 -0500

    ultimos cambios

--- StudentsWork/Estefa/WebPages_FrontEnd/JQuery/js/07.Read.js
+++ StudentsWork/Estefa/WebPages_FrontEnd/JQuery/js/07.Read.js
@@ -17,6 +17,14 @@ function CrearVariableGlobal(){
                 "Population" : 150.000,
                 "Status" : "Inactive"
             },
+            {
+                "Id" : 2203,
+                "Name" : "Brasil",
+                "LongName" : "Brasil's long Name",
+                "Description" : "South-american country",
+                "Population" : 150.000,
+                "Status" : "Inactive"
+            },
         ]
         $.each(listaJson,function(index, value){
             console.log(listaJson[index].Name)
