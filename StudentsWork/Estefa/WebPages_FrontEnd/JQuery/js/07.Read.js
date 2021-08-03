74871dd8aca0b0904955fdde5d2621bbf4e3457a
Estefa Rodriguez <estefa.rodriguez18@gmail.com>
2021-08-02 20:47:57 -0500

    Read 30 julio

--- StudentsWork/Estefa/WebPages_FrontEnd/JQuery/js/07.Read.js
+++ StudentsWork/Estefa/WebPages_FrontEnd/JQuery/js/07.Read.js
@@ -40,6 +40,7 @@ var listaJson = [
         "Status" : "Active"
     },
 ]
+// Aqui se hace lo del Read
 function LlenarTabla(){  
             $.each(listaJson,function(index, value){
             console.log(listaJson[index].Name)
@@ -51,3 +52,57 @@ function LlenarTabla(){
             $("#FormularioPaises").append(input);
         });
 }
+// Aqui se hace lo del Create
+function ClearField(){
+    $("#idField").val("");
+    $("#txtCountryName").val("");
+    $("#txtLongCountryName").val("");
+    $("#txtCountryDescription").val("");
+    $("#numPopulatioAprox").val("");
+    $("#radioActive").prop("checked",true);
+}
+function SaveField(){
+   var errorMsg = "";
+   var Id = parseInt($("#idField").val());
+   var CountryName = $("#txtCountryName").val();
+   var LonCountryName = $("#txtLongCountryName").val();
+   var CountryDescription = $("#txtCountryDescription").val();
+   var PopulationAprox = parseInt($("#numPopulatioAprox").val());
+   var radioActive =  $("input[name='Status']:checked").val();
+   if(Id < 0){
+       errorMsg += "Por favor el ID debe ser positivo \n"
+   }if(Id == ""){
+       errorMsg += "Por favor el ID debe tener informaición \n"
+   }if(CountryName.length > 15){
+       errorMsg += "Por favor Country name debe ser menor a 15 caracteres \n"
+   }if(CountryName == ""){
+       errorMsg += "Por favor Country name debe tener informaición \n"
+   }if(LonCountryName > 50){
+       errorMsg += "Por favor Long Country name debe ser menor a 50 caracteres \n"
+   }if(CountryDescription != ""){
+       if((CountryDescription.length < 9) || (CountryDescription >=250)){
+           errorMsg += "Por favor  Country descriptión debe ser mayot a 10 y  menor a 250 caracteres \n"
+       }
+   }if(errorMsg != ""){
+       swal(errorMsg);
+   }else{
+       var InfoForm = 
+           {
+               "Id" : Id,
+               "Name" : CountryName,
+               "LongName" : LonCountryName,
+               "Description" : CountryDescription,
+               "Population" : PopulationAprox,
+               "Status" : radioActive
+           }
+        var input = `<tr id="row_${InfoForm.Id}">
+        <td>${InfoForm.Name}</td>
+        <td>${InfoForm.Description}</td>
+        <td>${InfoForm.Status}</td>
+        </tr>`;
+        $("#FormularioPaises").append(input);
+        listaJson.push(InfoForm);// Adicionamos la nueva columna ingresada en la tabla
+        console.log(listaJson);
+   }
+}
+
