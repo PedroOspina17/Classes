10fe5d94f2d0d8ca7ff4538f1ec94f97c1228877
omar rendon <omar.1551-orb@gmail.com>
2021-08-03 00:10:42 -0500

    CountryDelete

--- StudentsWork/Omar/Web/03.JQuery/07.CountryCrud_Read.js
+++ StudentsWork/Omar/Web/03.JQuery/07.CountryCrud_Read.js
@@ -47,10 +47,11 @@ var info =  [
 
 function addRow(){
     $.each(info, function(index, value){
-        $("#table").append("<tr>" +
+        $("#table").append("<tr id='row_id'>" +
         "<td> " + value.Name + "</td >" +
         "<td>" + value.Description + "</td>" +
         "<td> " + value.Status + "</td>" +
+        "<td> " + "<input type = 'button' id='edit' class='btn btn-warning' value='Edit' onclick=''>" +"<input type = 'button' id='delete' class='btn btn-danger' value='Delete' onclick=''>" + "</td>" +
         "</tr>"
         )
         console.log(value);
@@ -58,11 +59,16 @@ function addRow(){
 }
 
 function msgValid(infoValidate){
-    if (infoValidate.Id == ""){
+    var validation = true;
+    
+
+    if (isNaN(infoValidate.Id) ){
         $("#idNumber").removeClass('text-hide');
+        validation = false;
     }
     else if(infoValidate.Id <= 0){
         $("#idNumber2").removeClass('text-hide');
+        validation = false;
     }
     else{
         $("#idNumber").addClass('text-hide');
@@ -71,9 +77,11 @@ function msgValid(infoValidate){
 
     if (infoValidate.Name == ""){
         $("#Name").removeClass('text-hide');
+        validation = false;
     }
     else if(infoValidate.Name.length > 15){
         $("#Name2").removeClass('text-hide');
+        validation = false;
     }
     else{
         $("#Name").addClass('text-hide');
@@ -82,6 +90,7 @@ function msgValid(infoValidate){
 
     if (infoValidate.LongName.length > 50){
         $("#long").removeClass('text-hide');
+        validation = false;
     }
     else{
         $("#long").addClass('text-hide');
@@ -89,16 +98,20 @@ function msgValid(infoValidate){
    
     if (infoValidate.Description.length < 10 && $("#countryDesc").val().length > 0){
         $("#countDesc").removeClass('text-hide');
+        validation = false;
     }
     else if(infoValidate.Description.length > 250){
         console.log($("#countryDesc").val().length)
         $("#countDesc2").removeClass('text-hide');
+        validation = false;
         console.log("entró");
     }
     else{
         $("#countDesc").addClass('text-hide');
         $("#countDesc2").addClass('text-hide');
     }
+
+    return validation;
     
 }
 
@@ -130,13 +143,20 @@ function create(){
             "Status": $("input[name = 'status']:checked").val()
         };
 
-    msgValid(formInfo);
-    $("#table").append("<tr>" +
-    "<td> " + formInfo.Name + "</td >" +
-    "<td>" + formInfo.Description + "</td>" +
-    "<td> " + formInfo.Status + "</td>" +
-    "</tr>")   
-    info.push(formInfo);
+    var validations = msgValid(formInfo);
+    
     console.log(info);
+    if (validations == true){
+        $("#table").append("<tr id='row_id'>" +
+        "<td> " + formInfo.Name + "</td >" +
+        "<td>" + formInfo.Description + "</td>" +
+        "<td> " + formInfo.Status + "</td>" +
+        "<td> " + "<input type = 'button' id='edit' class='btn btn-warning' value='Edit' onclick=''>" +"<input type = 'button' id='delete' class='btn btn-danger' value='Delete' onclick=''>" + "</td>" +
+        "</tr>")
+        toastr.success("The country was created!");
+        clearForm();
+        info.push(formInfo);
+    }
+    console.log(formInfo);
 }
 
