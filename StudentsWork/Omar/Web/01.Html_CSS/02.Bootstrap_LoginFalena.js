b8e428696e79d3a1913d1b9a2af59d3b1cfb2990
omar rendon <omar.1551-orb@gmail.com>
2021-08-25 22:47:11 -0500

    Login_and_Logout

--- StudentsWork/Omar/Web/01.Html_CSS/02.Bootstrap_LoginFalena.js
+++ StudentsWork/Omar/Web/01.Html_CSS/02.Bootstrap_LoginFalena.js
@@ -3,20 +3,31 @@ function save(){
     var email = $("#inputEmail").val();
     var password = $("#inputPass").val();
     if(email == ""){
-        msg += "The field 'E-mail' is required. \n"
+        msg += "The field 'Username' is required. \n"
     }
+    else if (email != "Admin" && email != "Admin2"){
+        msg += "Username incorrect! \n"
+    }
+
     if(password == ""){
-        msg += "The field 'Password' is required."
+        msg += "The field 'Password' is required. \n"
     }
     else if (password.length < 3){
-        msg += "The field 'Password' min 3 characters."
+        msg += "The field 'Password' min 3 characters. \n"
+    }
+    else if (password != "123" || password != "admin123"){
+        msg += "Password incorrect! \n"
+    }
+    if(email == "Admin" && password == "123"){
+        window.location.href="../../Web/03.JQuery/08.clientStorageList.html"
+        localStorage.setItem("Admin", "123")
+    }
+    else if(email == "Admin2" && password == "admin123"){
+        window.location.href="../../Web/03.JQuery/08.clientStorageList.html"
+        localStorage.setItem("Admin2", "admin123")
     }
 
     if(msg != ""){
         swal(msg);
-    }
-
-    if((email == "Admin" && password == "123") || (email == "Admin2" && password == "admin123")){
-        window.location.href="../../Web/03.JQuery/08.clientStorageList.html"
     }
 }
