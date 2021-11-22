aef942f8d8e642b19bb9aa9bd2a2212ee0201be8
Pedro Ospina <pedroospina17@gmail.com>
2021-11-22 11:48:08 -0500

    testing the py file

---
+++ ClassMaterial/PythonBasics/test.py
@@ -0,0 +1,20 @@
+def sumar(a,b):
+    print("sum")
+    print("sum")
+    print("sum")
+    return a+b
+
+print("adwdad")
+print("adwdad")
+print("adwdad")
+# a = input("escriba")
+# b= input("escriba")
+res = sumar(1,2)
+if(res==1):
+    print("tetst")
+else:
+    print("aaa")
+print("adwdad")
+
+
+
