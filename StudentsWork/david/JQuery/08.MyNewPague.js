d7c8f02f6c76665951ff59ef18a807ec73fe1a68
davidossa <davidossa0110@gmail.com>
2023-01-25 15:31:05 -0500

    paguina terminada y sin errores

--- StudentsWork/david/JQuery/08.MyNewPague.js
+++ StudentsWork/david/JQuery/08.MyNewPague.js
@@ -1,60 +1,114 @@
+function LeerLocalStorage() {
+    var Datos = localStorage.getItem("ListaProductos");
+    if (Datos != null) {
+        Datos = JSON.parse(Datos);
+        Datos.forEach(element => {
+            var FilaCreada =
+                `<tr id="${element.Id}" >
+            <td><button class="btn btn-outline-light" data-bs-target>${element.Marca}</button></td>
+            <td class="text-white">${element.Tipo}</td>
+            <td class="text-white">${element.Cantidad}</td>
+            <td class="btn-group">
+                <button class="btn btn-outline-warning" onclick="EditarFila('${element.Marca}','${element.Tipo}',${element.Cantidad})">Editar</button>
+                <button class="btn btn-outline-danger" onclick="EliminarFila(${element.Id})">Eliminar</button>
+            </td>
+        </tr>`
+            $("#CrearTabla").append(FilaCreada);
+            ContadorDeId = ContadorDeId + 1
+        });
+    }
+}
+
 var ContadorDeId = 0;
+var MarcaProducto
+var TipoProducto
+var CantidadProducto
+var DatosGuardados
 
 function SaveInformation() {
+    var Datos = localStorage.getItem("ListaProductos");
+    Datos = JSON.parse(Datos);
+    $.each(Datos, function (index,value){
+        if (value.Id > ContadorDeId){
+            ContadorDeId = value.Id
+        }
+    })
+    console.log("este es el id mas alto que hay:", ContadorDeId)
+    ContadorDeId = ContadorDeId + 1;
+
     if (document.getElementById("MarcaProducto").value == "" || document.getElementById("TipoProducto").value == "") {
         swal("CUIDADO", "No se puede dejar ningun campo vacio",
             "warning")
     }
     else {
-        var MarcaProducto = document.getElementById("MarcaProducto").value;
-        var TipoProducto = document.getElementById("TipoProducto").value;
-        var CantidadProducto = document.getElementById("CantidadProducto").value;
+        MarcaProducto = document.getElementById("MarcaProducto").value;
+        TipoProducto = document.getElementById("TipoProducto").value;
+        CantidadProducto = parseInt(document.getElementById("CantidadProducto").value);
+        DatosGuardados = {
+            "Marca": MarcaProducto,
+            "Tipo": TipoProducto,
+            "Cantidad": CantidadProducto,
+            "Id": ContadorDeId,
+        };
     };
-    //debugger
-    console.log(MarcaProducto);
-    console.log(TipoProducto);
-    console.log(CantidadProducto);
 
-    ContadorDeId = ContadorDeId + 1;
-    console.log("contador", ContadorDeId);
+    
+    
+    var DatosEnString = JSON.stringify(DatosGuardados);
+    
 
     var FilaCreada =
-
         `<tr id="${ContadorDeId}" >
-            <td><button class="btn btn-outline-light" data-bs-target>${MarcaProducto}</button></td>
-            <td class="text-white">${TipoProducto}</td>
-            <td class="text-white">${CantidadProducto}</td>
-            <td class="btn-group">
-                <button class="btn btn-outline-warning" onclick="EditarFila(${MarcaProducto,TipoProducto,CantidadProducto})">Editar</button>
-                <button class="btn btn-outline-danger" onclick="EliminarFila(${ContadorDeId})">Eliminar</button>
-            </td>
+           <td><button class="btn btn-outline-light" data-bs-target>${MarcaProducto}</button></td>
+           <td class="text-white">${TipoProducto}</td>
+          <td class="text-white">${CantidadProducto}</td>
+          <td class="btn-group">
+             <button class="btn btn-outline-warning" onclick="EditarFila('${MarcaProducto}',${TipoProducto}',${CantidadProducto}',${ContadorDeId})">Editar</button>
+             <button class="btn btn-outline-danger" onclick="EliminarFila(${ContadorDeId})">Eliminar</button>
+          </td>
         </tr>`
     $("#CrearTabla").append(FilaCreada);
-
     CancelAll();
-
-    console.log(ContadorDeId);
-
+    var Lista = JSON.parse(localStorage.getItem("ListaProductos"));
+    if (Lista != undefined) {
+        Lista.push(DatosGuardados);
+    }
+    else {
+        Lista = [
+            DatosGuardados
+        ]
+    }
+    localStorage.setItem("ListaProductos", JSON.stringify(Lista))
 }
+
 function CancelAll() {
-    console.log("entro al cancelar");
-    document.getElementById("MarcaProducto").value = "";
-    document.getElementById("TipoProducto").value = "";
-    document.getElementById("CantidadProducto").value = "";
+    $("#MarcaProducto").val("");
+    $("#TipoProducto").val("");
+    $("#CantidadProducto").val("");
 
 };
-function EditarFila(MarcaProducto,TipoProducto,CantidadProducto) {
-    console.log("entro al editar");
-    debugger;
+
+function EditarFila(MarcaProducto, TipoProducto, CantidadProducto, ContadorDeId) {
     $("#CantidadProducto").val(CantidadProducto);
     $("#TipoProducto").val(TipoProducto);
     $("#MarcaProducto").val(MarcaProducto);
+    console.log("variables retornadas dentro de la funcion editar...", MarcaProducto, TipoProducto, CantidadProducto)
     
-    
-    
-
 };
+
 function EliminarFila(IdEliminar) {
-    console.log("eliminado", IdEliminar);
+    var IndexEencontrado
+    var ReturnLocalStorage = JSON.parse(localStorage.getItem("ListaProductos"));
+    $.each(ReturnLocalStorage, function (index, value) {
+        var valorValue = value.Id
+        console.log("este es el valor de value", valorValue)
+        if (value.Id == IdEliminar) {
+            IndexEencontrado = index
+        }
+    })
+    ReturnLocalStorage.splice(IndexEencontrado, 1)
+    localStorage.setItem("ListaProductos", JSON.stringify(ReturnLocalStorage))
     $("#" + IdEliminar).remove();
+    console.log("eliminar fila", ReturnLocalStorage)
+    CancelAll()
 };
